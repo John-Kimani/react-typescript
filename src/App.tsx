b80681114f099a7f9645e93314ac6d48d5ecdeb8
John-Kimani <kimwaweru100@gmail.com>
2022-08-27 18:29:50 +0300

    App styles: Heading and main application styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello React TypeScript</p>
-    </div>
-  );
+const App: React.FC = () => {
+  return  <div className="App">
+    <span className='heading'>Taskify</span>
+  </div>;
 }
 
 export default App;
