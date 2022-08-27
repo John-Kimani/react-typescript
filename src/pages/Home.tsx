61367a29f99cd0c085e04180d8d9963be4042889
John-Kimani <kimwaweru100@gmail.com>
2022-08-27 22:17:45 +0300

    useNavigate function connected via link and button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export interface IHomePageProps {};
 
+
 const HomePage: React.FC<IHomePageProps> = (props) => {
+
+  const navigate = useNavigate();
+
+  
   return (
-    <div>HomePage</div>
+    <div>
+      <p>This is the home page</p>
+      <Link to="/about">Go to the About Page</Link>
+      <button onClick={()=> navigate('/layout/20')}>Go to layout, with a number</button>
+    </div>
+
   )
 }
 
