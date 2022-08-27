a79a97f18228652292a855e86aa257c8fb12ff35
John-Kimani <kimwaweru100@gmail.com>
2022-08-27 23:15:42 +0300

    Protect homepage

--- src/Application.tsx
+++ src/Application.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/Home'
 import LoginPage from './pages/Login'
 import firebaseApp from './config/config'
+import AuthRoute from './component/AuthRoute'
 
 export interface IApplicationProps {};
 
@@ -10,7 +11,9 @@ const Application: React.FC<IApplicationProps> = (props) => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<HomePage />} />
+      <Route path='/' element={<AuthRoute>
+        <HomePage />
+      </AuthRoute>} />
       <Route path='/login' element={<LoginPage />} />
     </Routes>
     </BrowserRouter>
