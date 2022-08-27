c947100022ddf251a81c5c442f620ceac06148f0
John-Kimani <kimwaweru100@gmail.com>
2022-08-27 19:07:55 +0300

    Model: Create and configure model to handle tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { ToDo } from './model';
 
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>('');
+  const [todos,setTodos] = useState<ToDo[]>([])
 
   console.log(todo)
 
