d4d4570473f3c02e5ba53a672ae0157c5466c4e6
John-Kimani <kimwaweru100@gmail.com>
2022-08-27 22:33:48 +0300

    Install router dom, Create homepage and routes form application instance

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/Home'
+import LoginPage from './pages/Login'
 
 export interface IApplicationProps {};
 
 const Application: React.FC<IApplicationProps> = (props) => {
   return (
-    <div>Application Entry file</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
