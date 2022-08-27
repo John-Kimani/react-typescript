2993b84624da21354a2479734da2a6ba8e4cd4ec
John-Kimani <kimwaweru100@gmail.com>
2022-08-27 21:18:06 +0300

    Sets: UseEffect hook to dispplay message and pass params

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 export interface IAboutPageProps {};
 
 const AboutPage: React.FC<IAboutPageProps> = (props) => {
+
+    const [ message, setMessage ] = useState('');
+    const { number} = useParams();
+
+    useEffect(() => {
+        if(number)
+        {
+            setMessage('The number is' + number);
+        }else {
+            setMessage("There was no number provided")
+        }
+    }, [])
+    
   return (
-    <div>AboutPage</div>
+    <div>
+        <p>AboutPage</p>
+        <p>{message}</p>
+    </div>
   )
 }
 
