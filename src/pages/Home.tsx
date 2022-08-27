131dcee49445f59edb00e283a70bf8173fbf7aac
John-Kimani <kimwaweru100@gmail.com>
2022-08-27 23:40:46 +0300

    Enable sign in with google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,10 @@ export interface IHomePageProps {};
 
 const HomePage: React.FC<IHomePageProps> = (props) => {
   return (
-    <div>HomePage</div>
+    <div>
+        <p>HomePage (Protected with Firebase!)</p>
+        <button>Sign out of Firebase</button>
+    </div>
   );
 };
 
