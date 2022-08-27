f5b6de2ab0b35b8ddddb39bec726c5f53c9e031e
John-Kimani <kimwaweru100@gmail.com>
2022-08-27 20:43:35 +0300

    Install firebase and create login page component

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//props as interface
+export interface ILoginPageProps {}
+
+const LoginPage: React.FunctionComponent<ILoginPageProps> = (props) => {
+  return (
+    <div>LoginPage</div>
+  )
+}
+
+export default LoginPage;
