42d90420038be185e99dedf28323306461226b9e
John-Kimani <kimwaweru100@gmail.com>
2022-08-27 22:11:17 +0300

    Create a layout hierachy

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AboutPage from './pages/About';
-import HomePage from './pages/Home';
-import LoginPage from './pages/Login';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LayoutComponent from "./components/Layout";
+import AboutPage from "./pages/About";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
 
-export interface IApplicationProps {};
+export interface IApplicationProps {}
 
 const Application: React.FC<IApplicationProps> = (props) => {
-  return <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<HomePage />} />
-    <Route path='/about'>
-        <Route index element={<AboutPage />} />
-        <Route path=':number' element={<AboutPage />} />
-        </Route> 
-    <Route path='/login' element={<LoginPage />} />
-  </Routes>
-  </BrowserRouter>;
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about">
+          <Route index element={<AboutPage />} />
+          <Route path=":number" element={<AboutPage />} />
+        </Route>
+        <Route path="/layout" element={<LayoutComponent />}>
+          <Route index element={<AboutPage />} />
+          <Route path=":number" element={<AboutPage />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default Application
+export default Application;
